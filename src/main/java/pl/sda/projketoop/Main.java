4e33b6294b207e3745a4747a3fe04b5d275ae306
SYTEKTOMASZ <59762629+SYTEKTOMASZ@users.noreply.github.com>
2020-03-25 18:56:51 +0100

    dodawanie nowego pracownika

--- src/main/java/pl/sda/projketoop/Main.java
+++ src/main/java/pl/sda/projketoop/Main.java
@@ -10,34 +10,78 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	Worker worker1  =   new Worker("Tomasz", "Dupa", 22, 'M', 1, 10000.00F, 2, true  );
-    Worker worker2  =   new Worker("Damian", "Wąsik", 18, 'M', 2, 200.00F, 0, false );
-    Worker worker3  =   new Worker("Kamil", "Zdun", 18, 'M', 2, 200.00F,0,false );
-        ArrayList<Worker> workerArrayList  =   new ArrayList<Worker>();
-workerArrayList.add(worker1);
-workerArrayList.add(worker2);
-workerArrayList.add(worker3);
+        Worker worker1 = new Worker("Tomasz", "Dupa", 22, 'M', 1, 10000.00F, 2, true);
+        Worker worker2 = new Worker("Damian", "Wąsik", 18, 'M', 2, 200.00F, 0, false);
+        Worker worker3 = new Worker("Kamil", "Zdun", 18, 'M', 2, 200.00F, 0, false);
+        ArrayList<Worker> workerArrayList = new ArrayList<Worker>();
+        workerArrayList.add(worker1);
+        workerArrayList.add(worker2);
+        workerArrayList.add(worker3);
+        int xyz = 0;
+        do {
+            int menu;
+            Scanner in = new Scanner(System.in);
+            System.out.println("Witaj");
+            System.out.println("Opcje:");
+            System.out.println("0 - koniec programu");
+            System.out.println("1 - wypisz pracowników");
+            System.out.println("2-Dodaj nowego pracownika");
+            menu = in.nextInt();
 
-
-    int menu;
-    Scanner in = new Scanner(System.in);
-    menu = in.nextInt();
-
-        switch (menu) {
-            case 1: {
-                for(Worker x : workerArrayList){
-                    x.printBasicInfo();
-                    System.out.println();
+            switch (menu) {
+                case 0:
+                {
+                    xyz =1;
+                    break;
                 }
-                break;
+                case 1: {
+                    for (Worker x : workerArrayList) {
+                        x.printBasicInfo();
+                        System.out.println();
+                    }
+                    break;
+                }
+                case 2: {
+                    Scanner newWorker = new Scanner(System.in);
+                    String name;
+                    String lastname;
+                    int age;
+                    char sex;
+                    int nr_dzial;
+                    float wage;
+                    int baby;
+                    boolean marry = false;
+                    System.out.println("Podaj Imie:");
+                    name = newWorker.nextLine();
+                    System.out.println("Podaj Nazwisko");
+                    lastname = newWorker.nextLine();
+                    System.out.println("Podaj wiek pracownika");
+                    age = newWorker.nextInt();
+                    System.out.println("Podaj płeć pracownika");
+                    sex = newWorker.next().charAt(0);
+                    System.out.println("Podaj numer działu");
+                    nr_dzial = newWorker.nextInt();
+                    System.out.println("Podaj pensje");
+                    wage = newWorker.nextFloat();
+                    System.out.println("Pdoaj ilość dzieci pracownika");
+                    baby = newWorker.nextInt();
+                    System.out.println("Jesli pracownik jest w zwiazku malzenskim 1 jesli cos innego");
+                    if (1 == newWorker.nextInt()) {
+                        marry = true;
+                    } else {
+                        marry = false;
+                    }
+                    workerArrayList.add(new Worker(name, lastname, age, sex, nr_dzial, wage, baby, marry));
+
+
+                    break;
+                }
             }
-            case 2:
-            {
-                System.out.println("2");
-                break;
-        }
 
         }
+        while(xyz==0);
+
+    }
 
 
 	/*
@@ -54,4 +98,4 @@ workerArrayList.add(worker3);
     */
     }
     
-}
+
